16fe1cef856455ad36ddf3b957b72f26a1c42c7c
ssakthivelsoftware <sakthivels912@gmail.com>
2022-03-22 14:37:40 -0700

    Mani

---
+++ src/org/git/Master.java
@@ -0,0 +1,15 @@
+package org.git;
+
+public class Master {
+private void car() {
+System.out.println("honda");
+}
+private void bike() {
+System.out.println("yamaha");
+}
+public static void main(String[] args) {
+	Master m= new  Master();
+	m.car();
+	m.bike();
+}
+}
